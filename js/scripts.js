aefce6cc9119a06c32ca5a64e4cae0cf2d22cb8b
Arnold Gerry <arnaud.gerry@gmail.com>
2022-03-07 07:13:20 +0300

    ft: defined names objects

---
+++ js/scripts.js
@@ -0,0 +1,5 @@
+//defining akan names object
+let akanNames = {
+    male: ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"],
+    female: ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"],
+};
